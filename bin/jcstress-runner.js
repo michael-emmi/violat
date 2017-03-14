b6abcce06236876cd994a2d379f6f45bfaa2d37f
Michael Emmi <michael.emmi@gmail.com>
2017-03-14 13:34:38 -0400

    Print message on passed tests.

--- bin/jcstress-runner.js
+++ bin/jcstress-runner.js
@@ -4,6 +4,8 @@ function main() {
   const proc = cp.spawn('java', ['-jar', 'jcstress/target/jcstress.jar', '-v', '-f', '1', '-iters', '1', '-jvmArgs', '-server']);
   const grep = cp.spawn('grep', ['FORBIDDEN', '-B', '10', '--line-buffered']);
 
+  var failed = false;
+
   proc.stdout.pipe(grep.stdin);
 
   proc.stdout.on('data', (data) => {
@@ -11,13 +13,19 @@ function main() {
   });
 
   grep.stdout.on('data', (data) => {
+    failed = true;
     var m = data.toString().match(/\[FAILED\] ([^ ]*)/);
 
     if (Array.isArray(m)) {
-      console.log(`failed: ${m[1]}`)
+      console.log(`test failed: ${m[1]}`)
       grep.kill();
       proc.kill();
     }
+  });
+
+  process.on('exit', _ => {
+    if (!failed)
+      console.log("All tests passed.")
   });
 }
 
