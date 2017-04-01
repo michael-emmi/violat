04e698b3a87048fe4db5a54b5821769a374ca899
Michael Emmi <michael.emmi@gmail.com>
2017-04-01 14:31:48 -0400

    Deterministic shuffles.

--- bin/shuffle.js
+++ bin/shuffle.js
@@ -1,8 +1,7 @@
 let seedrandom = require('seedrandom');
 
-seedrandom('knick-knacks', { global: true });
-
 module.exports = ary => {
+  seedrandom('anticonstitutionnellement', { global: true });
   for (let i = ary.length; i; i--) {
     let j = Math.floor(Math.random() * i);
     [ary[i - 1], ary[j]] = [ary[j], ary[i - 1]];
