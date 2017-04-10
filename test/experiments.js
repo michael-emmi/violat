1cfbaf5efcc927e4ae381417627dd33ae6368409
Michael Emmi <michael.emmi@gmail.com>
2017-04-10 10:31:00 -0400

    Command line flag passing.

--- test/experiments.js
+++ test/experiments.js
@@ -7,10 +7,6 @@ async function run(specDir) {
   let specFiles = cp.execSync(`find ${specDir} -name "*.json"`).toString().split('\n');
 
   for (let specFile of specFiles) {
-    let numValues = 2;
-    let sequences = 2;
-    let invocations = 4;
-
     console.log(`---`);
     console.log(`Checking specification: ${specFile}`);
     console.log(`---`);
