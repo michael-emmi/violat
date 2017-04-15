62e4b6eb88d15b5e81688470aafd5f4e7736d36f
Michael Emmi <michael.emmi@gmail.com>
2017-04-15 11:41:49 -0400

    Table gen optimizations.

--- test/tablegen.js
+++ test/tablegen.js
@@ -23,10 +23,12 @@ function formatHarness(code, method) {
       .map(line => line.replace(/result\.r. = ResultAdapter.get\((.*)\)/, '$1'))
       .map(line => line.trim().replace(/;/,''))
       .map(line => line.replace(/obj[.]/,''))
+      .map(line => line.replace(/offerLast/,'offer'))
+      .map(line => line.replace(/pollFirst/, 'poll'))
       .map(line => line.replace(/Arrays.asList\(([^)]*)\)/, '[$1]'))
       .map(line => line.replace(/Collections.unmodifiableMap\(Stream.of\(new AbstractMap.SimpleEntry<>\((.*),(.*)\), new AbstractMap.SimpleEntry<>\((.*),(.*)\)\).collect.*/,'\\{$1=$2,$3=$4\\})'))
-      .map(line => line.replace(method,'$\\diamond$'))
       .map(line => line.replace(/\s/,''))
+      .map(line => line.replace(new RegExp(`(${method})`),'{\\bf $1}'))
     );
 
   return actors.map(a => `[${a.join('; ')}]`).join(',');
@@ -42,8 +44,7 @@ function formatOutcome(code) {
 }
 
 function formatRow(data) {
-  return `${data.method}
-    & ${data.invocations}, ${data.sequences}, ${data.values}
+  return `${data.invocations}, ${data.sequences}, ${data.values}
     & ${data.explored.toLocaleString()} / ${data.total.toLocaleString()}
     & \\tt ${formatHarness(data.harness, data.method)}
     & \\tt ${formatOutcome(data.results[0].outcome)}
@@ -54,19 +55,19 @@ function formatRow(data) {
 
 function formatHeader(className, firstInTable) {
   if (firstInTable)
-    return `\\multicolumn{7}{c}{${className}} \\\\
-      & \\multicolumn{2}{c}{enumeration} & \multicolumn{3}{c}{failure} & \\\\
-      \\cmidrule(lr){2-3} \\cmidrule(lr){4-6}
-      method & \\#I, \\#S, \\#V & exp / gen & harness & outcome & frequency & time \\\\
-      \\cmidrule(lr){1-7}`.replace(/^\s+/mg,'');
+    return `\\multicolumn{6}{c}{${className}} \\\\
+      \\multicolumn{2}{c}{enumeration} & \\multicolumn{3}{c}{failure} & \\\\
+      \\cmidrule(lr){1-2} \\cmidrule(lr){3-5}
+      \\#I, \\#S, \\#V & exp / gen & harness & outcome & frequency & time \\\\
+      \\cmidrule(lr){1-6}`.replace(/^\s+/mg,'');
   else
     return `\\midrule
-      \\multicolumn{7}{c}{${className}} \\\\
-      \\cmidrule(lr){1-7}`.replace(/^\s+/mg,'');
+      \\multicolumn{6}{c}{${className}} \\\\
+      \\cmidrule(lr){1-6}`.replace(/^\s+/mg,'');
 }
 
 function frontMatter() {
-  return `\\begin{tabular}{lccllrr}
+  return `\\begin{tabular}{ccllrr}
     \\toprule`.replace(/^\s+/mg,'');
 }
 
