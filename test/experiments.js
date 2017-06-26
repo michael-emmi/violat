e40b7433e60a2e72ba17d19ab14edd12e4737a02
Michael Emmi <michael.emmi@gmail.com>
2017-06-26 16:35:12 -0400

    Updated experiments script.

--- test/experiments.js
+++ test/experiments.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var cp = require('child_process');
-let config = require('../lib/config.js');
-let checker = require('../lib/index.js');
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+const config = require('../lib/config.js');
+const checker = require('../lib/index.js');
 
 async function run(specDir) {
   let specFiles = cp.execSync(`find ${specDir} -name "*.json"`).toString().split('\n');
@@ -12,7 +13,8 @@ async function run(specDir) {
     console.log(`---`);
 
     let result = await checker.testUntrustedMethods({
-      spec: specFile
+      specFile: specFile,
+      spec: JSON.parse(fs.readFileSync(specFile))
     });
 
     console.log(`---`);
