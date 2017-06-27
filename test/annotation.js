29702368c4c67b5779f4f2ba5e70bf3cef42bf5d
Michael Emmi <michael.emmi@gmail.com>
2017-06-27 12:14:07 -0400

    Increasing timeout so travis can deal with gradle.

--- test/annotation.js
+++ test/annotation.js
@@ -4,8 +4,8 @@ const generator = require('../lib/enumeration');
 const annotate = require('../lib/annotation');
 const spec = require('../resources/specs/java/util/concurrent/ConcurrentHashMap.json');
 
-describe.skip('annotate()', function() {
-  this.timeout(5000);
+describe('annotate()', function() {
+  this.timeout(50000);
 
   const n = 5;
   const schemaGenerator = generator({
