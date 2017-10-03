5680c9d6fadb73a3b8ffe616fd29ff936c5b3e9e
Michael Emmi <michael.emmi@gmail.com>
2017-10-03 00:12:34 -0400

    Collecting weak outcomes.

--- bin/cli.js
+++ bin/cli.js
@@ -20,6 +20,7 @@ let cli = meow(`
     --invocations N             Total number of invocations.
     --enum [complete|shuffle|random]  Enumeration strategy (default: ${defaults.enum}).
     --limit N                   Limit to N violations (default: ${defaults.limit}).
+    --cutoff N                  Explore at most N harnesses (default: ${defaults.cutoff}).
     --weak                      Check “weak” atomicity.
 
   Examples
@@ -45,17 +46,25 @@ let cli = meow(`
   console.log(`class: ${args.spec.class}`);
   console.log(`---`);
 
-  let violations = args.method
+  let results = args.method
     ? await checker.testMethod(args)
     : await checker.testUntrustedMethods(args);
 
-  for (let violation of violations) {
-    console.log(`Violation discovered in the following harness.`);
+  for (let result of results) {
+    if (result.outcomes.every(x => x.expectation == 'ACCEPTABLE' || x.count < 1))
+      continue;
+
+    console.log(`Violation/weakness discovered in the following harness.`);
     console.log(`---`);
-    console.log(violation.harnessCode);
+    console.log(result.harness);
     console.log(`---`);
-    for (let r of violation.forbiddenResults)
-      console.log(`${r.count} of ${violation.numExecutions} executions gave outcome: ${r.outcome}`);
+    let total = result.outcomes.reduce((sum,x) => sum + x.count, 0);
+    for (let outcome of result.outcomes) {
+      if (outcome.expectation == 'FORBIDDEN')
+        console.log(`${outcome.count} of ${total} executions gave violating outcome: ${outcome.result}`);
+      else if (outcome.expectation == 'ACCEPTABLE_INTERESTING')
+        console.log(`${outcome.count} of ${total} executions gave weak(${outcome.description}) outcome: ${outcome.result}`);
+    }
     console.log(`---`);
   }
 })();
