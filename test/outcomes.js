b573ea3a60df847fdbf2a793862202bb948d21cb
Michael Emmi <michael.emmi@gmail.com>
2017-10-18 10:34:20 -0400

    Merged redundant tests harnesses.

--- test/outcomes.js
+++ test/outcomes.js
@@ -23,6 +23,13 @@ async function get(schema, opts) {
   return (await outcomes([schema], opts || ATOMIC))[0].outcomes.map(o => o.values);
 }
 
+function testcase(schema, opts, expected) {
+  return {
+    compute: async () => new Set(await get(schema, opts)),
+    expected: new Set(expected)
+  };
+}
+
 const ATOMIC = {};
 const WEAK = {weak: true};
 const WEAKEST = {
@@ -32,22 +39,32 @@ const WEAKEST = {
   weakRelaxReturns: true
 };
 
-const S1 = schema(
-  'java.util.concurrent.ConcurrentHashMap',
-  [ ['clear', [], [], false],
-    ['put', ['java.lang.Object','java.lang.Object'], [0,1], true] ],
-  [ ['containsKey', ['java.lang.Object'], [1], true],
-    ['remove', ['java.lang.Object'], [0], true] ] );
+const T1 = testcase(
+  schema('java.util.concurrent.ConcurrentHashMap',
+    [ ['clear', [], [], false],
+      ['put', ['java.lang.Object','java.lang.Object'], [0,1], true] ],
+    [ ['containsKey', ['java.lang.Object'], [1], true],
+      ['remove', ['java.lang.Object'], [0], true] ] ),
+  WEAKEST,
+  [ ['null', 'null', 'false', '1'],
+    ['null', 'null', 'false', 'null'] ] );
 
-const OS1 = [
-  ['null', 'null', 'false', '1'],
-  ['null', 'null', 'false', 'null']
-];
+const T2 = testcase(
+  schema('java.util.concurrent.ConcurrentHashMap',
+    [ ['put', ['java.lang.Object','java.lang.Object'], [0,0], true],
+      ['put', ['java.lang.Object','java.lang.Object'], [1,0], true],
+      ['remove', ['java.lang.Object'], [1], true] ],
+    [ ['put', ['java.lang.Object','java.lang.Object'], [0,1], true],
+      ['clear', [], [], false] ] ),
+    WEAKEST,
+    [ ['null', 'null', 'false', '1'],
+      ['null', 'null', 'false', 'null'] ] );
 
 describe('outcomes', function() {
   this.timeout(50000);
 
   it (`outcomes() reflects the correct outcomes`, async function() {
-    assert.deepEqual(new Set(await get(S1, WEAKEST)), new Set(OS1));
+    assert.deepEqual(await T1.compute(), T1.expected);
+    assert.deepEqual(await T2.compute(), T2.expected);
   });
 });
