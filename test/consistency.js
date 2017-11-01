d6f074f638ae2ee68ba3a9ec768aa0203aff24a9
Michael Emmi <michael.emmi@gmail.com>
2017-10-31 21:12:06 -0400

    Immutable consistencies.

--- test/consistency.js
+++ test/consistency.js
@@ -30,64 +30,66 @@ const V1 = new PartialOrder();
 V1.sequence(I1, I3);
 V1.add(I2);
 
-let C1 = new Consistency();
-C1.integrate(RELATIONS.linearization, RELATIONS.programorder, L1, P1, I1, I2);
-C1.integrate(BASIC_LEVELS.consistentreturns, I3);
+let C1 = Consistency.full();
+C1 = C1.weakenRelationalLevel(RELATIONS.linearization, RELATIONS.programorder, L1, P1, I1, I2);
+C1 = C1.weakenSimpleLevel(BASIC_LEVELS.consistentreturns, I3);
 
-let C2 = new Consistency();
-C2.integrate(RELATIONS.linearization, RELATIONS.programorder, L2, P1, I1, I2);
+let C2 = Consistency.full();
+C2 = C2.weakenRelationalLevel(RELATIONS.linearization, RELATIONS.programorder, L2, P1, I1, I2);
 
-let C3 = new Consistency();
-C3.integrate(RELATIONS.linearization, RELATIONS.programorder, L3, P1, I1, I2);
-C3.integrate(RELATIONS.visibility, RELATIONS.linearization, V1, L3, I1, I2);
+let C3 = Consistency.full();
+C3 = C3.weakenRelationalLevel(RELATIONS.linearization, RELATIONS.programorder, L3, P1, I1, I2);
+C3 = C3.weakenRelationalLevel(RELATIONS.visibility, RELATIONS.linearization, V1, L3, I1, I2);
+
+let linIncludesPo = Consistency.level(
+  RELATIONS.linearization, RELATIONS.programorder,
+  COMPOSITIONS.simple,
+  EXPRESSIONS.wildcard, EXPRESSIONS.wildcard);
+
+let linIncludesPoB = Consistency.level(
+  RELATIONS.linearization, RELATIONS.programorder,
+  COMPOSITIONS.simple,
+  EXPRESSIONS.bottom, EXPRESSIONS.bottom);
+
+let visIndludesPo = Consistency.level(
+  RELATIONS.visibility, RELATIONS.programorder,
+  COMPOSITIONS.simple,
+  EXPRESSIONS.wildcard, EXPRESSIONS.wildcard);
+
+let visIndludesLin = Consistency.level(
+  RELATIONS.visibility, RELATIONS.linearization,
+  COMPOSITIONS.simple,
+  EXPRESSIONS.wildcard, EXPRESSIONS.wildcard);
+
+let consistentReturns = Consistency.level(
+  BASIC_LEVELS.consistentreturns, EXPRESSIONS.wildcard);
+
+let consistentReturnsA = Consistency.level(
+  BASIC_LEVELS.consistentreturns, EXPRESSIONS.atomic);
 
 describe('consistency', function() {
 
   it (`basic inclusion`, function() {
-    assert.ok(C1.includes(Consistency.level(
-      RELATIONS.linearization, RELATIONS.programorder,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.wildcard, EXPRESSIONS.wildcard)));
-
-    assert.ok(C2.includes(Consistency.level(
-      BASIC_LEVELS.consistentreturns, EXPRESSIONS.atomic)));
-
-    assert.ok(C3.includes(Consistency.level(
-      BASIC_LEVELS.consistentreturns, EXPRESSIONS.atomic)));
+    assert.ok(C1.includes(linIncludesPo));
+    assert.ok(C2.includes(consistentReturnsA));
+    assert.ok(C3.includes(consistentReturnsA));
   });
 
   it (`basic exclusion`, function() {
-    assert.ok(C1.includes(Consistency.level(
-      BASIC_LEVELS.consistentreturns, EXPRESSIONS.atomic)));
-
-    assert.ok(!C1.includes(Consistency.level(
-      BASIC_LEVELS.consistentreturns, EXPRESSIONS.wildcard)));
-
-    assert.ok(C2.includes(Consistency.level(
-      RELATIONS.linearization, RELATIONS.programorder,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.bottom, EXPRESSIONS.bottom)));
-
-    assert.ok(!C2.includes(Consistency.level(
-      RELATIONS.linearization, RELATIONS.programorder,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.atomic, EXPRESSIONS.atomic)));
+    assert.ok(C1.includes(consistentReturnsA));
+    assert.ok(!C1.includes(consistentReturns));
+    assert.ok(C2.includes(linIncludesPoB));
+    assert.ok(!C2.includes(linIncludesPo));
   });
 
   it (`mixed levels`, function() {
-    assert.ok(C3.includes(Consistency.level(
-      RELATIONS.linearization, RELATIONS.programorder,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.wildcard, EXPRESSIONS.wildcard)));
+    assert.ok(C3.includes(linIncludesPo));
+    assert.ok(C3.includes(visIndludesPo));
+    assert.ok(!C3.includes(visIndludesLin));
+  });
 
-    assert.ok(C3.includes(Consistency.level(
-      RELATIONS.visibility, RELATIONS.programorder,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.wildcard, EXPRESSIONS.wildcard)));
-
-    assert.ok(!C3.includes(Consistency.level(
-      RELATIONS.visibility, RELATIONS.linearization,
-      COMPOSITIONS.simple,
-      EXPRESSIONS.wildcard, EXPRESSIONS.wildcard)));
+  it (`simplification`, function() {
+    assert.ok(C1.simplify().includes(linIncludesPo));
+    assert.ok(!C1.simplify().includes(consistentReturns));
   });
 });
