4f028e94b50112d8a3a7c41e2bb8b46677551afd
Michael Emmi <michael.emmi@gmail.com>
2018-01-23 22:29:09 -0500

    Generating histories from program enumeration.

--- bin/cli-histories.js
+++ bin/cli-histories.js
@@ -15,18 +15,25 @@ let name = Object.keys(meta.bin)
 const { Schema } = require(path.join(__dirname, '../lib', 'schema.js'));
 const annotate = require(path.join(__dirname, '../lib', 'outcomes.js'));
 const { JCStressHistoryGenerator } = require(path.join(__dirname, '../lib', 'jcstress.js'));
+const enumeration = require(path.join(__dirname, '../lib', 'random-enumeration.js'));
 
 const uuidv1 = require('uuid/v1');
 
 let cli = meow(`
   Usage
-    $ ${name} <harness-schema.json>
+    $ ${name} <spec-file.json>
 
   Options
-    --schema STRING             Construct schema from STRING and JSON spec.
+    --schema STRING
+    --methods <method-name(s)>
+    --values N
+    --sequences N
+    --invocations N
+    --cutoff N
 
   Examples
     $ ${name} --schema "{ clear(); put(0,1) } || { containsKey(1); remove(0) }" ConcurrentHashMap.json
+    $ ${name} --methods clear ConcurrentHashMap.json
 `, {
   boolean: [],
   default: {}
@@ -37,34 +44,47 @@ let cli = meow(`
   if (cli.input.length !== 1)
     cli.showHelp();
 
-  let json = fs.readFileSync(cli.input[0]);
-  let schema = cli.flags.schema
-    ? Schema.fromString(cli.flags.schema, json)
-    : Schema.fromJson(json);
+  if (!cli.flags.schema && !cli.flags.methods)
+    cli.showHelp();
 
-  if (!schema.id)
-    schema.id = 0;
-  let args = Object.assign({}, cli.flags);
+  let spec = JSON.parse(fs.readFileSync(cli.input[0]));
+  let args = Object.assign({}, defaults, cli.flags, {
+    spec,
+    methods: cli.flags.methods && cli.flags.methods.split(',')
+  });
 
   console.log(`${cli.pkg.name} version ${cli.pkg.version}`);
 
-  let tester = new JCStressHistoryGenerator([schema], 'Blah');
-  let testResults = await tester.run();
-  let total = testResults[0].total;
-  let histories = testResults[0].histories;
+  let schemas = [];
+  let total = 0;
 
-  console.log(`observed ${histories.length} outcomes in ${total} executions`);
-  console.log(`---`);
+  for (let schema of enumeration.generator(args)) {
+    schemas.push(schema);
+    if (schemas.length < 100 && !(args.cutoff <= ++total))
+      continue;
 
-  for (let history of histories) {
-    let hpath = path.join(config.historiesPath, ...history.schema.class.split('.'), `${uuidv1()}.json`);
-    mkdirp(path.dirname(hpath), (err) => {
-      if (err) throw err;
-      fs.writeFile(hpath, JSON.stringify(history), (err) => {
-        if (err) throw err;
-      });
-    });
-    console.log(`${history}`);
-    console.log(`---`);
+    let tester = new JCStressHistoryGenerator(schemas, 'History');
+
+    for (let result of await tester.run()) {
+      console.log(`observed ${result.histories.length} histories in ${result.total} executions`);
+      console.log(`---`);
+
+      for (let history of result.histories) {
+        let hpath = path.join(config.historiesPath, ...history.schema.class.split('.'), `${uuidv1()}.json`);
+        mkdirp(path.dirname(hpath), (err) => {
+          if (err) throw err;
+          fs.writeFile(hpath, JSON.stringify(history), (err) => {
+            if (err) throw err;
+          });
+        });
+        console.log(`${history}`);
+        console.log(`---`);
+      }
+    }
+
+    if (args.cutoff <= total)
+      break;
+
+    schemas = [];
   }
 })();
